c985af28a573b8f968e5286cbf3c6a35e1f466f1
Todd, Andy <andy.todd@adidas.com>
2022-07-20 12:19:33 +0200

    Added name searching

--- net/searchpoc/Controllers/SearchController.cs
+++ net/searchpoc/Controllers/SearchController.cs
@@ -24,25 +24,42 @@ namespace searchpoc.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetSearch(string articleId)
+        public IActionResult GetSearch(string? articleId,string? name)
         {            
-            var results = DoSearch(articleId);
+            var results = DoSearch(articleId,name);
             return new JsonResult(results);
         }
 
-        private IReadOnlyCollection<object> DoSearch(string articleId)
+        private IReadOnlyCollection<object> DoSearch(string? articleId,string? name)
         {
             var client = GetESClient();
+
+
+            MatchPhraseQueryDescriptor<object> queryDescriptor = new MatchPhraseQueryDescriptor<object>();
+            
+
+            if(!string.IsNullOrEmpty(articleId))
+            {
+                queryDescriptor
+                    .Field("id")
+                    .Query(articleId);                                   
+            }
+
+            if(!string.IsNullOrEmpty(name))
+            {
+                queryDescriptor
+                    .Field("name")
+                    .Query("*" + name + "*");
+            }
+
             
             var searchResult = client.Search<object>(s => s
                 .Query(
-                    q => q
-                    .Match(m => m
-                        .Field("id")
-                        .Query(articleId)
+                    q => q.MatchPhrase(q => queryDescriptor)                    
                     )
-                )
+               .AnalyzeWildcard(true)
             );
+            
 
             var products = searchResult.Documents;
             return products;
